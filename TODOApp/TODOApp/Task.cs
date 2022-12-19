49b2d6744cffa9a386c606abe57d09c538acdd87
carsonSgit <92652800+carsonSgit@users.noreply.github.com>
2022-12-19 00:56:57 -0500

    Adding Task class

---
+++ TODOApp/TODOApp/Task.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TODOApp
+{
+    class Task
+    {
+        private string _notes;
+        private string _name;
+        private DateTime _dueDate;
+
+        #region Properties
+        public string Notes
+        {
+            get
+            {
+                return _notes;
+            }
+            set
+            {
+                _notes = value;
+            }
+        }
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+        public DateTime Date
+        {
+            get
+            {
+                return _dueDate;
+            }
+            set
+            {
+                _dueDate = value;
+            }
+        }
+        #endregion
+    }
+}
