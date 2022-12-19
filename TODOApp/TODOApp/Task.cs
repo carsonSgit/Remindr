3231c85f204039524863d02e7d04342aef51ccf4
XULIM <67230458+XULIM@users.noreply.github.com>
2022-12-19 03:10:33 -0500

    Constructors added

--- TODOApp/TODOApp/Task.cs
+++ TODOApp/TODOApp/Task.cs
@@ -6,9 +6,37 @@ namespace TODOApp
 {
     class Task
     {
+        #region Fields
+
         private string _notes;
         private string _name;
         private DateTime _dueDate;
+
+        #endregion
+
+        #region Constructors
+
+        public Task()
+        {
+            Name = "Sample Task";
+            Notes = String.Empty;
+            Date = DateTime.Now;
+        }
+
+        public Task(string name, DateTime dueDate)
+        {
+            Name = name;
+            Date = dueDate;
+        }
+
+        public Task(string name, DateTime dueDate, string notes)
+        {
+            Name = name;
+            Date = dueDate;
+            Notes = notes;
+        }
+
+        #endregion
 
         #region Properties
         public string Notes
